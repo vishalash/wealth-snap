22b92dd11ce983100b430f9f88f36cc0badb29bb
vishalash <vishalashx2@gmail.com>
2023-04-21 13:27:49 +0530

    better login in mobile

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -13,7 +13,7 @@ const Login = () => {
     dispatch(login(localStorage.getItem('userName')));
   }
   return (
-    <div className="bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    <div className="bg-gray-100 py-6 flex justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-light-blue-500 shadow-lg transform skew-y-0 rotate-6 sm:rotate-12 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
