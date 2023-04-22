d504c6c4c6e5daf3eaf556562751e43b04c409e0
vishalash <vishalashx2@gmail.com>
2023-04-22 16:52:41 +0530

    adding google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,23 @@ import '../styles/global.css';
 import { Provider } from 'react-redux';
 import store from '../store/store';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-DRSB8XNFLM"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-DRSB8XNFLM');
+        `}
+    </Script>
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
